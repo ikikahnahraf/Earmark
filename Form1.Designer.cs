44d0beecbde0619b2684c5241cdc2036f2c29bc3
FH40082X <Farhan.Hakiki@cimbniaga.co.id>
2024-02-22 15:12:08 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Earmark_BCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importFromSIBSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transactionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transferToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Nama = new System.Windows.Forms.Label();
+            this.NIK = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Cari = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Transaksi = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(138, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 53);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processToolStripMenuItem,
+            this.transactionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1250, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importFromSIBSToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // importFromSIBSToolStripMenuItem
+            // 
+            this.importFromSIBSToolStripMenuItem.Name = "importFromSIBSToolStripMenuItem";
+            this.importFromSIBSToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.importFromSIBSToolStripMenuItem.Text = "Import from SIBS";
+            // 
+            // transactionToolStripMenuItem
+            // 
+            this.transactionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paymentToolStripMenuItem,
+            this.transferToolStripMenuItem});
+            this.transactionToolStripMenuItem.Name = "transactionToolStripMenuItem";
+            this.transactionToolStripMenuItem.Size = new System.Drawing.Size(116, 29);
+            this.transactionToolStripMenuItem.Text = "Transaction";
+            // 
+            // paymentToolStripMenuItem
+            // 
+            this.paymentToolStripMenuItem.Name = "paymentToolStripMenuItem";
+            this.paymentToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.paymentToolStripMenuItem.Text = "Payment";
+            // 
+            // transferToolStripMenuItem
+            // 
+            this.transferToolStripMenuItem.Name = "transferToolStripMenuItem";
+            this.transferToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.transferToolStripMenuItem.Text = "Transfer";
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nama.Location = new System.Drawing.Point(12, 56);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(126, 46);
+            this.Nama.TabIndex = 3;
+            this.Nama.Text = "Nama";
+            // 
+            // NIK
+            // 
+            this.NIK.AutoSize = true;
+            this.NIK.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NIK.Location = new System.Drawing.Point(12, 123);
+            this.NIK.Name = "NIK";
+            this.NIK.Size = new System.Drawing.Size(85, 46);
+            this.NIK.TabIndex = 4;
+            this.NIK.Text = "NIK";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(138, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(305, 53);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Cari
+            // 
+            this.Cari.ContextMenuStrip = this.contextMenuStrip1;
+            this.Cari.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cari.Location = new System.Drawing.Point(459, 50);
+            this.Cari.Name = "Cari";
+            this.Cari.Size = new System.Drawing.Size(163, 123);
+            this.Cari.TabIndex = 7;
+            this.Cari.Text = "Cari";
+            this.Cari.UseVisualStyleBackColor = true;
+            this.Cari.Click += new System.EventHandler(this.Cari_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1205, 285);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Transaksi
+            // 
+            this.Transaksi.ContextMenuStrip = this.contextMenuStrip1;
+            this.Transaksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Transaksi.Location = new System.Drawing.Point(975, 484);
+            this.Transaksi.Name = "Transaksi";
+            this.Transaksi.Size = new System.Drawing.Size(250, 123);
+            this.Transaksi.TabIndex = 9;
+            this.Transaksi.Text = "Transaksi";
+            this.Transaksi.UseVisualStyleBackColor = true;
+            this.Transaksi.Click += new System.EventHandler(this.Transaksi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 614);
+            this.Controls.Add(this.Transaksi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Cari);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.NIK);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importFromSIBSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transactionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paymentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transferToolStripMenuItem;
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label NIK;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Cari;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Transaksi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Earmark_BCP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cari_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Transaksi_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
